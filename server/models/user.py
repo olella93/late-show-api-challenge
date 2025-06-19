7836fde606f615276d5461fbe257ce7cc2f05185
Richard <olella93@gmail.com>
2025-06-20 01:37:21 +0300

    implement User model with username and password auth

---
+++ server/models/user.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import validates
+from werkzeug.security import generate_password_hash, check_password_hash
+from server.config import db
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = generate_password_hash(password)
+
+    def authenticate(self, password):
+        return check_password_hash(self._password_hash, password)
+
+    def __repr__(self):
+        return f"<User {self.username}>"

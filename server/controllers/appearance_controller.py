d12aa09637b6914354da60825c4cbb943f3bd61e
Richard <olella93@gmail.com>
2025-06-20 02:17:24 +0300

    implement appearances routes with rating validation

--- server/controllers/appearance_controller.py
+++ server/controllers/appearance_controller.py
@@ -1,7 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from server.models.appearance import Appearance
+from server.models import db
 
-appearance_bp = Blueprint('appearance_bp', __name__)
+appearance_bp = Blueprint('appearance_bp', __name__, url_prefix='/appearances')
 
-@appearance_bp.route("/appearances", methods=["GET"])
+@appearance_bp.route('/', methods=['GET'])
 def get_appearances():
-    return {"message": "Placeholder for appearances"}
+    appearances = Appearance.query.all()
+    return jsonify([appearance.to_dict() for appearance in appearances]), 200
+
+@appearance_bp.route('/', methods=['POST'])
+def create_appearance():
+    data = request.get_json()
+    new_appearance = Appearance(
+        rating=data['rating'],
+        guest_id=data['guest_id'],
+        episode_id=data['episode_id']
+    )
+    db.session.add(new_appearance)
+    db.session.commit()
+    return jsonify(new_appearance.to_dict()), 201

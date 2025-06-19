7826b103f4e2a9011737caf1721265b188357d7d
Richard <olella93@gmail.com>
2025-06-20 01:39:16 +0300

    create Guest model with id, name, occupation, and appearances

---
+++ server/models/guest.py
@@ -0,0 +1,13 @@
+from server.config import db
+
+class Guest(db.Model):
+    __tablename__ = "guests"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    occupation = db.Column(db.String)
+
+    appearances = db.relationship('Appearance', back_populates='guest', cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f"<Guest {self.name}>"

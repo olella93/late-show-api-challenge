f5656104065854b8cc2de57b6c09a6456bb43f7b
Richard <olella93@gmail.com>
2025-06-20 02:18:38 +0300

    add JWT authentication with login endpoint

--- server/controllers/auth_controller.py
+++ server/controllers/auth_controller.py
@@ -1,3 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from server.models.user import User
+from server.models import db
+from flask_jwt_extended import create_access_token
+from werkzeug.security import check_password_hash
 
-auth_bp = Blueprint('auth', __name__)
+auth_bp = Blueprint('auth_bp', __name__, url_prefix='/auth')
+
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+
+    if user and check_password_hash(user.password_hash, data['password']):
+        token = create_access_token(identity=user.id)
+        return jsonify(access_token=token), 200
+    return jsonify(error="Invalid credentials"), 401

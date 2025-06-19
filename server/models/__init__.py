5c37307fe95a976b6175271f39b6946995160137
Richard <olella93@gmail.com>
2025-06-20 01:49:09 +0300

    organize model imports in central file

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,1 +1,5 @@
-from server.app import db
+from server.config import db
+from server.models.user import User
+from server.models.guest import Guest
+from server.models.episode import Episode
+from server.models.appearance import Appearance

581c3befa61071ecba61876349c25c123dea92f2
Richard <olella93@gmail.com>
2025-06-20 01:17:50 +0300

    appearance_bp updated

--- server/controllers/appearance_controller.py
+++ server/controllers/appearance_controller.py
@@ -1,3 +1,7 @@
 from flask import Blueprint
 
 appearance_bp = Blueprint('appearance_bp', __name__)
+
+@appearance_bp.route("/appearances", methods=["GET"])
+def get_appearances():
+    return {"message": "Placeholder for appearances"}
